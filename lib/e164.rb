b36a25708b40a3ddbd1b8252081aac46c462650e
Collin <collin@remind101.com>
2014-09-11 13:46:33 -0700

    Make prefix search actually work properly

--- lib/e164.rb
+++ lib/e164.rb
@@ -3,7 +3,7 @@ require 'e164/lookuptable'
 
 module E164
   def self.lookup(number)
-    for cut_idx in 1..number.length
+    for cut_idx in (number.length-1).downto(0)
       cut = number[0..cut_idx]
       if E164::LookupTable.include?(cut)
         return E164::LookupTable[cut]
