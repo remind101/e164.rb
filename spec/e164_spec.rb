aa7e4748ec36d5049e207b5ef45e09356069d972
Tor Helmer <torhelmer@gmail.com>
2020-02-10 21:56:01 -0800

    tests

--- spec/e164_spec.rb
+++ spec/e164_spec.rb
@@ -3,11 +3,19 @@ require 'e164'
 describe E164 do
   describe '#lookup' do
     it 'can look up a phone number' do
-      expect(E164.lookup("14037238282")).to eq(["CA", "Canada"])
+      expect(E164.lookup('14037238282')).to eq(['CA', 'Canada'])
     end
 
     it 'doesn\'t flail with a malformed phone number' do
-      expect(E164.lookup("@remindhq")).to eq(nil)
+      expect(E164.lookup('@remindhq')).to eq(nil)
+    end
+    it 'respects 689 as valid us country code' do
+      expect(E164.lookup('16891234567')).to eq(['US', 'United States'])
+    end
+
+    it 'respects 787 and 939 as Puerto Rico' do
+      expect(E164.lookup('17871234567')).to eq(['PR', 'Puerto Rico'])
+      expect(E164.lookup('19391234567')).to eq(['PR', 'Puerto Rico'])
     end
   end
 end
